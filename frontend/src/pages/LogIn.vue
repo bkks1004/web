063f77a9fc9f47265ea41e73c15523d3b5c05003
oskim <bkks1004@naver.com>
2023-10-10 21:12:13 +0900

    로그인 창 input에서 enter 적용

--- frontend/src/pages/LogIn.vue
+++ frontend/src/pages/LogIn.vue
@@ -6,76 +6,93 @@
         style="width: 200px; height: 200px"
       > -->
     <q-card style="height: 600px; width: 400px">
-      <h6 class="text-center q-mb-sm" style="font-family: 'ONE-Mobile-Title'">
-        Welcome
-      </h6>
-      <h5 class="text-center q-mt-sm" style="font-family: 'ONE-Mobile-Title'">
-        Login
-      </h5>
-      <div class="row basic-row">
-        <div class="col">
-          <q-input v-model="state.id" label="User ID">
-            <template #prepend>
-              <q-icon name="account_circle" />
-            </template>
-          </q-input>
+      <q-form>
+        <h6 class="text-center q-mb-sm" style="font-family: 'ONE-Mobile-Title'">
+          Welcome
+        </h6>
+        <h5 class="text-center q-mt-sm" style="font-family: 'ONE-Mobile-Title'">
+          Login
+        </h5>
+        <div class="row basic-row">
+          <div class="col">
+            <q-input
+              v-model="state.id"
+              label="User ID"
+              @keydown.enter.prevent="focusPassword"
+            >
+              <template #prepend>
+                <q-icon name="account_circle" />
+              </template>
+            </q-input>
+          </div>
         </div>
-      </div>
-      <div class="basic-row">
-        <div class="col">
-          <q-input
-            v-model="state.password"
-            label="Password"
-            :type="isPwd ? 'password' : 'text'"
-            hind="Password"
-            :rules="[val => !!val || '비밀번호를 입력해주세요.']"
-            lazy-rules
-          >
-            <template #prepend>
-              <q-icon name="password" />
-            </template>
-            <template #append>
-              <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
-            </template>
-          </q-input>
+        <div class="basic-row">
+          <div class="col">
+            <q-input
+              v-model="state.password"
+              name="password"
+              label="Password"
+              :type="isPwd ? 'password' : 'text'"
+              hind="Password"
+              :rules="[val => !!val || '비밀번호를 입력해주세요.']"
+              lazy-rules
+              @keydown.enter.prevent="login"
+            >
+              <template #prepend>
+                <q-icon name="password" />
+              </template>
+              <template #append>
+                <q-icon
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
+                />
+              </template>
+            </q-input>
+          </div>
         </div>
-      </div>
-      <div class="basic-row justify-center">
-        <q-btn
-          class="glossy full-width q-mt-md"
-          rounded
-          color="primary"
-          size="md"
-          label="Login"
-          @click="login"
-        />
-      </div>
-      <div class="basic-row justify-center">
-        <q-btn
-          class="full-width q-my-sm"
-          rounded
-          color="white"
-          text-color="black"
-          size="md"
-          label="Join"
-          @click="join"
-        />
-      </div>
-      <div class="row basic-row justify-between q-mx-sm">
-        <p style="cursor: pointer" @click="join">회원가입</p>
-        <p>아이디 / 비밀번호 찾기</p>
-      </div>
-      <div class="basic-row justify-center text-center">
-        <img
-          src="../assets/naver-logo-button.png"
-          width="140"
-          @click="naverLogin"
-        />
-      </div>
+        <div class="basic-row justify-center">
+          <q-btn
+            class="glossy full-width q-mt-md"
+            rounded
+            color="primary"
+            size="md"
+            label="Login"
+            @click="login"
+          />
+        </div>
+        <div class="basic-row justify-center">
+          <q-btn
+            class="full-width q-my-sm"
+            rounded
+            color="white"
+            text-color="black"
+            size="md"
+            label="Join"
+            @click="join"
+          />
+        </div>
+        <div class="row basic-row justify-between q-mx-sm">
+          <p style="cursor: pointer" @click="join">회원가입</p>
+          <p>아이디 / 비밀번호 찾기</p>
+        </div>
+        <div class="basic-row justify-center text-center">
+          <img
+            src="../assets/naver-logo-button.png"
+            width="140"
+            height="37"
+            style="cursor: pointer; margin-right: 25px"
+            @click="naverLogin"
+          />
+          <img
+            src="../assets/kakao_login_medium_narrow.png"
+            width="140"
+            height="37"
+            style="cursor: pointer"
+            @click="kakaoLogin"
+          />
+        </div>
+      </q-form>
     </q-card>
   </q-page>
 </template>
@@ -108,7 +125,11 @@ export default defineComponent({
     function join() {
       router.push({ path: '/join' })
     }
-
+    function focusPassword() {
+      if (document.getElementsByName('password')) {
+        document.getElementsByName('password')[0].focus()
+      }
+    }
     const naverLogin = () => {
       const url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NAVER_CLIENT_ID}&state=${process.env.NAVER_STATE}&redirect_uri=${process.env.NAVER_CALLBACK_URL}`
       window.location.href = url
@@ -119,6 +140,7 @@ export default defineComponent({
       state,
       login,
       join,
+      focusPassword,
       naverLogin,
     }
   },
