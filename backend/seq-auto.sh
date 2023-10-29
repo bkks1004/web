752053a98f6bbd23735730c29dc27a771009f8a7
ohseong_kim <bkks1004@naver.com>
2023-10-29 16:22:42 +0900

    seq-auto 설정 변경

--- backend/seq-auto.sh
+++ backend/seq-auto.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sequelize-auto -o "./models" -d my_db -h localhost -u root -p 3306 -x test123 -e mysql --noInitModels;
+sequelize-auto -o "./models" -d my_db -h localhost -u root -p 3306 -x test123 -e mysql --cm c --cf c --noInitModels;
